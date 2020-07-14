1dd149524ccce15c92f94e5dcea061415c3f137c
Aakash <aakash@MacBook-Pro.local>
2020-07-14 10:03:15 +0530

    Adding implementation for Heap sort

--- Heap/MaxHeap.java
+++ Heap/MaxHeap.java
@@ -6,29 +6,30 @@ public class MaxHeap {
         heap[j] = temp;
     }
 
-    private static void heapify(int[] heap, int i) {
+    public static void heapify(int[] heap, int i, int n) {
         int left = 2*i + 1;
         int right = 2*i + 2;
         int largest = i;
-        if(left < heap.length && heap[left] > heap[i])
+        if(left < n && heap[left] > heap[i])
             largest = left;
-        if(right < heap.length && heap[right] > heap[largest])
+        if(right < n && heap[right] > heap[largest])
             largest = right;
         if(largest != i) {
             swap(heap, largest, i);
-            heapify(heap, largest);
+            heapify(heap, largest, n);
         }
     }
 
-    private static void buildMaxHeap(int[] heap) {
+    public static void buildMaxHeap(int[] heap) {
         for(int i = (heap.length)/2 - 1; i >= 0; i--) {
-            heapify(heap, i);
+            heapify(heap, i, heap.length);
         }
 
     }
     public static void main(String[] args) {
         int[] heap = new int[]{1,2,3,4,5,6};
         buildMaxHeap(heap);
+        System.out.println("Max heap array is:");
         for(int x : heap) {
             System.out.print(x + " ");
         }
